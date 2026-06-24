07fe422b8a9b4249d5778b302725733d703af504
Ned Bingham <edward.bingham@broccolimicro.io>
2026-06-24 07:49:40 -0400

    adding export functions for function declarations

--- interpret_wv/export.cpp
+++ interpret_wv/export.cpp
@@ -1,6 +1,52 @@
 #include <interpret_wv/export.h>
+#include <interpret_arithmetic/export.h>
 
 namespace weaver {
+
+parse_ucs::function::declaration export_declaration(const Program &prgm, const Instance &inst) {
+	parse_ucs::function::declaration result;
+	result.valid = true;
+
+	result.type = export_type_name(prgm, inst.type);
+	parse_ucs::function::declaration::variable var;
+	var.name = inst.name;
+	for (int sz : inst.size) {
+		var.size.push_back(arithmetic::export_expression<parse_ucs::expression>(arithmetic::Value(sz)));
+	}
+
+	result.name.push_back(var);
+
+	return result;
+}
+
+parse_ucs::type_name export_type_name(const Program &prgm, TypeId id) {
+	parse_ucs::type_name result;
+	if (not prgm.typeValid(id)) {
+		return result;
+	}
+
+	result.mod = prgm.modAt(id).name;
+	result.name = prgm.typeAt(id).name;
+	return result;
+}
+
+parse_ucs::function_decl export_decl(const Program &prgm, const Decl &decl) {
+	parse_ucs::function_decl result;
+	result.valid = true;
+
+	result.name = decl.name;
+	if (prgm.typeValid(decl.recv)) {
+		result.recv = prgm.typeAt(decl.recv).name;
+	}
+
+	result.ret = export_type_name(prgm, decl.ret);
+
+	for (const auto &arg : decl.args) {
+		result.args.push_back(export_declaration(prgm, arg));
+	}
+
+	return result;
+}
 
 parse_ucs::modfile export_modfile(const Project &proj) {
 	parse_ucs::modfile result;
--- interpret_wv/export.h
+++ interpret_wv/export.h
@@ -1,10 +1,18 @@
 #pragma once
 
 #include <parse_ucs/modfile.h>
+#include <parse_ucs/type_name.h>
+#include <parse_ucs/function_decl.h>
+#include <parse_ucs/function.h>
 
 #include <weaver/project.h>
+#include <weaver/program.h>
 
 namespace weaver {
+
+parse_ucs::function::declaration export_declaration(const Program &prgm, const Instance &inst);
+parse_ucs::type_name export_type_name(const Program &prgm, TypeId id);
+parse_ucs::function_decl export_decl(const Program &prgm, const Decl &decl);
 
 parse_ucs::modfile export_modfile(const Project &proj);
 
